cb3d2188be4bd0225377a17e6145af16a9fb710c
Brad Yeoman <16478567+BradYeoman@users.noreply.github.com>
2022-12-04 14:31:41 +0000

    Update streamlit_app.py
    indexes the 'Fruit' column so customers can choose fruit by name instead of index number

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ st.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🍇')
 
 # load csv for fruits into pandas and display the dataframe
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # add a pick list here so users can pick the fruit
 # they want to include in their smoothie

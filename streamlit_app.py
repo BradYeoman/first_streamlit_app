32c9544a24be0df18f6cb662cd6e7d1ba3965ff0
Brad Yeoman <16478567+BradYeoman@users.noreply.github.com>
2022-12-04 14:34:33 +0000

    Update streamlit_app.py
    pre-populates the pick list with some example selections

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # add a pick list here so users can pick the fruit
 # they want to include in their smoothie
-st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # display the table on the page
 st.dataframe(my_fruit_list)

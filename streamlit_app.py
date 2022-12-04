fb3778cde6fbd6e63f19df9304cb6e3b57a77e34
Brad Yeoman <16478567+BradYeoman@users.noreply.github.com>
2022-12-04 14:20:12 +0000

    Update streamlit_app.py
    adds fruit data from csv
    creates dataframe of fruit in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
+# import packages needed
 import streamlit as st
+import pandas as pd
 
+# create main menu items
 st.title('My Parent\'s New Healthy Diner')
 
 st.header('Breakfast Favorites')
@@ -9,3 +12,7 @@ st.text('🐔 🥚 Hard-Boiled Free-Range Egg')
 st.text('🥑 🍞 Avocado Toast')
 
 st.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🍇')
+
+# load csv for fruits into pandas and display the dataframe
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)

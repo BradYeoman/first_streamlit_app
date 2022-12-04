c4270e4c1bd069e12a1e5d396a3a535107bdf245
Brad Yeoman <16478567+BradYeoman@users.noreply.github.com>
2022-12-04 14:39:10 +0000

    Update streamlit_app.py
    adds fruits_selected and fruits_to_show variables to filter the dataframe based on user selections

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,8 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # add a pick list here so users can pick the fruit
 # they want to include in their smoothie
-st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # display the table on the page
-st.dataframe(my_fruit_list)
+st.dataframe(fruits_to_show)
